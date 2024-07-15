f8c497a4d19879f9ba96edcf6e837a1442ab69c5
x <x@DESKTOP-D57KLT3>
2024-07-15 13:48:06 -0300

    Add project files.

---
+++ AwesomeDevEvents.API/Controllers/DevEventsController.cs
@@ -0,0 +1,173 @@
+﻿using AutoMapper;
+using AwesomeDevEvents.API.Entities;
+using AwesomeDevEvents.API.Models;
+using AwesomeDevEvents.API.Persistence;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace AwesomeDevEvents.API.Controllers
+{
+    [Route("api/dev-events")]
+    [ApiController]
+    public class DevEventsController : ControllerBase
+    {
+        private readonly DevEventsDbContext _context;
+        private readonly IMapper _mapper;
+        public DevEventsController(
+            DevEventsDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        /// <summary>
+        /// Obter todos os eventos
+        /// </summary>
+        /// <returns>Coleção de eventos</returns>
+        /// <response code="200">Sucesso</response>
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public IActionResult GetAll()
+        {
+            var devEvents = _context.DevEvents.Where(d => !d.IsDeleted).ToList();
+
+            var viewModel = _mapper.Map<List<DevEventViewModel>>(devEvents);
+
+            return Ok(viewModel);
+        }
+
+        /// <summary>
+        /// Obter um evento
+        /// </summary>
+        /// <param name="id">Identificador do evento</param>
+        /// <returns>Dados do evento</returns>
+        /// <response code="200">Sucesso</response>
+        /// <response code="404">Não encontrado</response>
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult GetById(Guid id)
+        {
+            var devEvent = _context.DevEvents
+                .Include(de => de.Speakers)
+                .SingleOrDefault(d => d.Id == id);
+            if (devEvent == null)
+            {
+                return NotFound();
+            }
+
+            var viewModel = _mapper.Map<DevEventViewModel>(devEvent);
+
+            return Ok(viewModel);
+        }
+
+        /// <summary>
+        /// Cadastrar um evento
+        /// </summary>
+        /// <remarks>
+        /// Objeto JSON
+        /// </remarks>
+        /// <param name="input">Dados de evento</param>
+        /// <returns>Ojeto recém-criado</returns>
+        /// <response code="201">Sucesso</response>
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        public IActionResult Post(DevEventInputModel input)
+        {
+            var devEvent = _mapper.Map<DevEvent>(input);
+            _context.DevEvents.Add(devEvent);
+            _context.SaveChanges();
+
+            return CreatedAtAction(nameof(GetById), new { id = devEvent.Id }, devEvent);
+
+        }
+        /// <summary>
+        /// Atualizar um evento
+        /// </summary>
+        /// <remarks>
+        /// Objeto JSON
+        /// </remarks>
+        /// <param name="id">Identiicador do evento</param>
+        /// <param name="Input">Dados do evento</param>
+        /// <returns>Nada.</returns>
+        /// <response code="404">Não encontrado.</response>
+        /// <response code="204">Sucesso</response>
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public IActionResult Update(Guid id, DevEventInputModel Input)
+        {
+            //throw NotImplementedException();
+            var devEvent = _context.DevEvents.SingleOrDefault(d => d.Id == id);
+            
+            if ( devEvent == null)
+            {
+                return NotFound();
+            }
+
+            devEvent.Update(Input.Title, Input.Description, Input.StartDate, Input.EndDate);
+
+            _context.DevEvents.Update(devEvent);
+            _context.SaveChanges();
+
+            return NoContent();
+
+        }
+
+        /// <summary>
+        /// Deletar um evento
+        /// </summary>
+        /// <param name="id">Identificador de evento</param>
+        /// <returns>Nada</returns>
+        /// <response code="404">Não encontrado</response>
+        /// <response code="204">Sucesso</response>
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public IActionResult Delete(Guid id)
+        {
+            var devEvent = _context.DevEvents.SingleOrDefault(d => d.Id == id);
+            if (devEvent == null )
+            {
+                return NotFound();
+            }
+
+            devEvent.Delete();
+
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+        /// <summary>
+        /// Cadastrar palestrante
+        /// </summary>
+        /// <remarks>
+        /// objeto JSON
+        /// </remarks>
+        /// <param name="id">Identificador do evento</param>
+        /// <param name="input">Dados do palestrante</param>
+        /// <returns>Nada</returns>
+        /// <response code="404">Não encontrado</response>
+        /// <responde code="204">Evento não encontrado</responde>
+        [HttpPost("{id}/speakers")]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public IActionResult PostSpeaker(Guid id, DevEventSpeakerInputModel input)
+        {
+            var speaker = _mapper.Map<DevEventSpeaker>(input);
+
+            speaker.DevEventId = id;
+            var devEvent = _context.DevEvents.Any(d => d.Id == id);
+            if (!devEvent)
+            {
+                return NotFound();
+            }
+
+            _context.DevEventSpeakers.Add(speaker);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+    }
+}
